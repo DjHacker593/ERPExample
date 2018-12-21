ca3ad1260c1e6fde30ca4d45d44155b57f787406
Alexander V. García <40124784+DjHacker593@users.noreply.github.com>
2018-12-20 21:03:36 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="refresh" content="1;url=home">
+    <title>
+      ERPExample
+    </title>
+  </head>
+  
+  <body>
+    Cargando...
+  </body>
+</html>
